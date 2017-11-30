cd43b6b4a6d261efcbdbf7fee9ae86e67ed433da
Antony <Toxa23A@gmail.com>
2017-11-30 03:55:33 +0200

    add NonNull anotation

--- app/src/main/java/com/community/jboss/compressorhead/utils/CompressorHead.java
+++ app/src/main/java/com/community/jboss/compressorhead/utils/CompressorHead.java
@@ -13,7 +13,7 @@ public class CompressorHead {
     public static final String JPEG = "jpeg";
     public static final String WEBP = "webp";
 
-    public static String getURL(@NonNull String imageURl, int width, int height, String format) {
+    public static String getURL(@NonNull String imageURl, @NonNull int width, int height, String format) {
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("https")
                 .authority(Constants.BASE_URL)
